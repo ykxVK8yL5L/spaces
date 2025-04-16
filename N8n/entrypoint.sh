18ce68163eb8483daa6536b684810668e6311c5e
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2025-04-16 14:54:29 +0800

    Create entrypoint.sh

---
+++ N8n/entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+
+
+echo -e "======================写入rclone配置========================\n"
+echo "$RCLONE_CONF" > ~/.config/rclone/rclone.conf
+
+
+if [ -n "$RCLONE_CONF" ]; then
+  echo -e "##########同步备份############"
+  # 指定远程文件夹路径，格式为 remote:path
+  REMOTE_FOLDER="n8n:/"
+
+  # 使用 rclone ls 命令列出文件夹内容，将输出和错误分别捕获
+  OUTPUT=$(rclone ls "$REMOTE_FOLDER" 2>&1)
+
+  # 获取 rclone 命令的退出状态码
+  EXIT_CODE=$?
+
+  # 判断退出状态码
+  if [ $EXIT_CODE -eq 0 ]; then
+    # rclone 命令成功执行，检查文件夹是否为空
+    if [ -z "$OUTPUT" ]; then
+      #为空不处理
+      #rclone sync --interactive /ql $REMOTE_FOLDER
+      echo "初次安装"
+    else
+      rclone sync $REMOTE_FOLDER /root/.n8n
+    fi
+  elif [[ "$OUTPUT" == *"directory not found"* ]]; then
+    echo "错误：文件夹不存在"
+  else
+    echo "错误：$OUTPUT"
+  fi
+else
+    echo "没有检测到Rclone配置信息"
+fi
+
+n8n start
