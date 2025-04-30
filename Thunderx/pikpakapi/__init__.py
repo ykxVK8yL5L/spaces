e31c7e6bf0b956db0045ce97a39ae3149e15b42c
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2025-05-01 07:14:04 +0800

    如发生错误返回错误信息

--- Thunderx/pikpakapi/__init__.py
+++ Thunderx/pikpakapi/__init__.py
@@ -241,6 +241,9 @@ class PikPakApi:
             await self.refresh_access_token()
             raise PikpakRetryException("Token refreshed, please retry")
 
+        if json_data.get("error_description") is not None:
+            return json_data.get("error_description")
+
         raise PikpakException(json_data.get("error_description", "Unknown Error"))
 
     async def _request_get(
@@ -908,7 +911,6 @@ class PikPakApi:
         )
         return result
 
-  
     async def get_quota_info(self) -> Dict[str, Any]:
         """
         获取当前空间的quota信息
@@ -962,7 +964,6 @@ class PikPakApi:
         result = await self._request_get(url)
         return result
 
-
     async def file_batch_share(
         self,
         ids: List[str],
@@ -987,8 +988,8 @@ class PikPakApi:
         data = {
             "file_ids": ids,
             "share_to": "copy",
-            "restore_limit":"-1",
-            "expiration_days":"-1",
+            "restore_limit": "-1",
+            "expiration_days": "-1",
             "expiration_days": expiration_days,
             "pass_code_option": "REQUIRED" if need_password else "NOT_REQUIRED",
         }
@@ -1037,13 +1038,12 @@ class PikPakApi:
         data = {
             "limit": "100",
             "thumbnail_size": "SIZE_SMALL",
-            "page_token":next_page_token,
+            "page_token": next_page_token,
         }
         url = f"https://{self.PIKPAK_API_HOST}/drive/v1/share/list"
         captcha_result = await self.captcha_init(f"GET:/drive/v1/share/list")
         self.captcha_token = captcha_result.get("captcha_token")
         return await self._request_get(url, params=data)
-
 
     async def get_share_folder(
         self, share_id: str, pass_code_token: str, parent_id: str = None
