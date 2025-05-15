e381b908f79b1b49b70a9f4ae0ff37bda64beebf
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2025-05-15 13:55:53 +0800

    Update main.go

--- halalcloud/main.go
+++ halalcloud/main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"embed"
 	"halalcloud/handlers"
 	"halalcloud/routes"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+var content embed.FS
+
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.File("templates/index.html")
+		data, err := content.ReadFile("templates/index.html")
+		if err != nil {
+			c.String(http.StatusInternalServerError, "Error loading index.html")
+			return
+		}
+		// 返回 HTML 文件的内容
+		c.Data(http.StatusOK, "text/html", data)
 	})
 	r.GET("/login", handlers.Login)
 	routes.SetupRoutes(r)
