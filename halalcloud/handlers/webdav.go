eb763ca3cad34af5326004d0984edd41762549c3
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2025-05-15 23:42:14 +0800

    Update webdav.go

--- halalcloud/handlers/webdav.go
+++ halalcloud/handlers/webdav.go
@@ -175,6 +175,47 @@ func DeleteWebdav(c *gin.Context) {
 	c.JSON(http.StatusOK, result)
 }
 
+func CreateWebdav(c *gin.Context) {
+	serv, exists := c.Get("authService")
+	if !exists {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "authService not found"})
+		return
+	}
+	fserv, ok := serv.(*auth.AuthService)
+	if !ok {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "认证服务出错！！！",
+		})
+		return
+	}
+
+	var webdavRequestBody pbDavConfig.DavConfig
+	// 从请求体中解析 JSON 到 requestBody 结构体
+	if err := c.ShouldBindJSON(&webdavRequestBody); err != nil {
+		// 如果绑定失败，返回 400 错误
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "无效的请求数据",
+		})
+		return
+	}
+	client := pbDavConfig.NewPubDavConfigClient(fserv.GetGrpcConnection())
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+
+	result, err := client.Create(ctx, &webdavRequestBody)
+
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, result)
+}
+
 func ListAllWebdav(c *gin.Context) {
 	serv, exists := c.Get("authService")
 	if !exists {
