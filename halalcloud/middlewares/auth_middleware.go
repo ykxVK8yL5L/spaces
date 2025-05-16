6bcff178eee42a588819d499a7e850deb4ede010
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2025-05-16 10:28:20 +0800

    Update auth_middleware.go

--- halalcloud/middlewares/auth_middleware.go
+++ halalcloud/middlewares/auth_middleware.go
@@ -4,6 +4,7 @@ import (
 	"halalcloud/auth"
 	"halalcloud/constants"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -19,7 +20,23 @@ func AuthMiddleware() gin.HandlerFunc {
 			c.Abort()
 			return
 		}
-		serv, err := auth.NewAuthService(constants.AppID, constants.AppVersion, constants.AppSecret, refreshToken)
+
+		accessToken := ""
+		var accessTokenExpiredAt int64 = 0
+		accessToken = c.GetHeader("Access-Token")
+
+		expiredAtStr := c.GetHeader("Access-Token-Expired-At")
+		if expiredAtStr != "" {
+			var err error
+			accessTokenExpiredAt, err = strconv.ParseInt(expiredAtStr, 10, 64)
+			if err != nil {
+				// 如果转换失败，返回错误响应
+				c.JSON(400, gin.H{"error": "Invalid Access-Token-Expired-At"})
+				return
+			}
+		}
+
+		serv, err := auth.NewAuthService(constants.AppID, constants.AppVersion, constants.AppSecret, refreshToken, accessToken, accessTokenExpiredAt)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{
 				"message": err.Error(),
@@ -28,6 +45,17 @@ func AuthMiddleware() gin.HandlerFunc {
 			return
 		}
 		c.Set("authService", serv)
+		// println(serv.GetAuth())
+
+		// userJson, err := json.Marshal(serv.GetAuth())
+		// if err != nil {
+		// 	c.JSON(http.StatusInternalServerError, gin.H{
+		// 		"message": "无法获取用户信息",
+		// 	})
+		// 	c.Abort()
+		// 	return
+		// }
+
 		//client := pubUserFile.NewPubUserFileClient(serv.GetGrpcConnection())
 		//c.Set("userClient", client)
 
