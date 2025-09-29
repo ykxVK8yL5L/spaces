8d63b1c8e6301b10ebdacc22b4aa9c7521d14303
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2025-09-29 17:31:14 +0800

    允许n8n调用所有外部模块

--- n8n.py
+++ n8n.py
@@ -120,6 +120,7 @@ Check out the configuration reference at https://huggingface.co/docs/hub/spaces-
             {"key": "GENERIC_TIMEZONE", "value": "Asia/Shanghai"},
             {"key": "TZ", "value": "Asia/Shanghai"},
             {"key": "NODE_FUNCTION_ALLOW_BUILTIN", "value": "*"},
+            {"key": "N8N_NODE_FUNCTION_ALLOW_EXTERNAL", "value": "*"},
             {"key": "N8N_ENFORCE_SETTINGS_FILE_PERMISSIONS", "value": "true"},
             {"key": "WEBHOOK_URL", "value": f"https://{userid}-{space_name}.hf.space"},
         ],
