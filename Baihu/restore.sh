663b16be97f40238374a652a75434461e067ee35
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2026-02-25 05:50:25 +0000

    暂时这样 等待白虎官方完善恢复功能

--- Baihu/restore.sh
+++ Baihu/restore.sh
@@ -1,14 +1,30 @@
 
-echo -e "======================写入rclone配置========================\n"
+npm install pm2 -g
+#开启白虎服务
+pm2 start ./baihu --name baihu
+
+echo "10秒后开始恢复任务..."
+sleep 10
+
+#从日志中获取密码
+echo  "======================从日志中获取密码========================\n"
+DEFAULT_PASSWORD=$(tail -n 100 ~/.pm2/logs/baihu-out.log \
+    | grep -oP '密\s*码:\s*\K[^,[:space:]]+' \
+    | tail -n 1)
+
+echo  "默认用户名: admin"
+echo  "默认密码: $DEFAULT_PASSWORD"
+
+
+echo  "======================写入rclone配置========================\n"
 echo "$RCLONE_CONF" > ~/.config/rclone/rclone.conf
 
-if [ -n "$RCLONE_CONF" ]; then
-  echo -e "##########同步备份############"
 
- 
+if [ -n "$RCLONE_CONF" ]; then
+  echo "##########同步备份############"
+
   # 使用 rclone ls 命令列出文件夹内容，将输出和错误分别捕获
   OUTPUT=$(rclone ls "$REMOTE_FOLDER" 2>&1)
-
   # 获取 rclone 命令的退出状态码
   EXIT_CODE=$?
 
@@ -21,8 +37,24 @@ if [ -n "$RCLONE_CONF" ]; then
       #为空不处理
       echo "初次安装"
     else
+      # 获取响应Token
+      BHToken=$(
+      curl -s -D - -o /dev/null \
+        'http://localhost:8052/api/v1/auth/login' \
+        -H 'content-type: application/json' \
+        --data-raw "{\"username\":\"admin\",\"password\":\"$DEFAULT_PASSWORD\"}" \
+      | awk -F'[=;]' '/Set-Cookie: BHToken=/{print $2}'
+      )
+      echo $BHToken
+
       #echo "文件夹不为空"
-      rclone sync $REMOTE_FOLDER /app --exclude="/baihu" --exclude "/docker-entrypoint.sh"
+      # rclone sync $REMOTE_FOLDER /app --exclude="/baihu" --exclude "/docker-entrypoint.sh"
+      mkdir /app/backups
+      # 找最新的文件名
+      latest_file=$(rclone lsjson remote:source | jq -r 'sort_by(.ModTime) | last | .Path')
+      # 复制到目标目录
+      rclone copy remote:source/$latest_file /app/backups
+      ls /app/backups
     fi
   elif [[ "$OUTPUT" == *"directory not found"* ]]; then
     echo "错误：文件夹不存在"
@@ -32,4 +64,6 @@ if [ -n "$RCLONE_CONF" ]; then
 else
     echo "没有检测到Rclone配置信息"
 fi
+
+tail -f /dev/null
 
