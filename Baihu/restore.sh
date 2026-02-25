38557acf1e37346da4326f68055cce1662afc1c4
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2026-02-25 03:13:50 +0000

    添加rclone输出错误代码

--- Baihu/restore.sh
+++ Baihu/restore.sh
@@ -4,12 +4,16 @@ echo "$RCLONE_CONF" > ~/.config/rclone/rclone.conf
 
 if [ -n "$RCLONE_CONF" ]; then
   echo -e "##########同步备份############"
+
+ 
   # 使用 rclone ls 命令列出文件夹内容，将输出和错误分别捕获
   OUTPUT=$(rclone ls "$REMOTE_FOLDER" 2>&1)
 
   # 获取 rclone 命令的退出状态码
   EXIT_CODE=$?
 
+  echo "rclone退出代码:$EXIT_CODE"
+  
   # 判断退出状态码
   if [ $EXIT_CODE -eq 0 ]; then
     # rclone 命令成功执行，检查文件夹是否为空
